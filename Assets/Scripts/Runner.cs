a206a0b476159079f58998ea6c14e141fcf51458
Sitan Gao <sitangao@usc.edu>
2019-05-16 16:19:11 -0700

    Add a platform counter

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Runner : MonoBehaviour {
     private float jumpHeight = 1250f;
-    private float newPlatformCenterX = 11.5f;
+    private float newPlatformCenterX;
+    private float nextDetectionPointX;
     private float platformCenterY;
     private bool isGround;
     private float preTime;
-    public GameObject newPlatform, platform;
+    private int score;
+    public Text display;
+    public GameObject newPlatform;
     // Use this for initialization
     void Start() {
         GameObject firstPlatform = GameObject.Find("FirstPlatform");
         platformCenterY = firstPlatform.transform.position.y;
         firstPlatform.GetComponent<MeshRenderer>().material.color = Color.blue;
         gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+        newPlatformCenterX = 11.5f;
+        nextDetectionPointX = 10f;
         isGround = false;
         preTime = 0f;
+        score = 0;
     }
 	
 	// Update is called once per frame
@@ -66,7 +73,7 @@ public class Runner : MonoBehaviour {
     {
         if(newPlatformCenterX - gameObject.transform.position.x < 15)
         {
-            platform = (GameObject)Instantiate(newPlatform, new Vector3(newPlatformCenterX, platformCenterY, 0F), Quaternion.identity);
+            GameObject platform = (GameObject)Instantiate(newPlatform, new Vector3(newPlatformCenterX, platformCenterY, 0F), Quaternion.identity);
             newPlatformCenterX = newPlatformCenterX + 5;
             float randomNumber = Random.value;
             if (randomNumber < 0.5)
@@ -92,6 +99,12 @@ public class Runner : MonoBehaviour {
         if (gameObject.GetComponent<MeshRenderer>().material.color != collision.gameObject.GetComponent<MeshRenderer>().material.color)
         {
             Die();
+        }
+        else if(gameObject.transform.position.x > nextDetectionPointX)
+        {
+            score++;
+            display.text = "No of platform: " + score.ToString();
+            nextDetectionPointX = nextDetectionPointX + 5;
         }
     }
     public IEnumerator GameOver()
