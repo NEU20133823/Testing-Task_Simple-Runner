6c7ad21dbc0725ff48cdea5f9c357d30fd730b02
Sitan Gao <sitangao@usc.edu>
2019-05-15 19:19:45 -0700

    First Commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+    private GameObject runner;
+    private float xoffset;
+    // Use this for initialization
+    void Start () {
+        runner = GameObject.FindGameObjectWithTag("Runner");
+        xoffset = gameObject.transform.position.x - runner.transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        gameObject.transform.position = new Vector3(runner.transform.position.x+xoffset, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Runner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner : MonoBehaviour {
+    private float jumpHeight = 1500f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(3, 0, 0);
+        gameObject.transform.Translate(move * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+    }
+}
