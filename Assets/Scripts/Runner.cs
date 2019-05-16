0d5b226550de81795e5fb2524b430a85628242c9
Sitan Gao <sitangao@usc.edu>
2019-05-16 00:23:05 -0700

    Generate endless platform and adjust moving and jumping parameter

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Runner : MonoBehaviour {
-    private float jumpHeight = 1500f;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float jumpHeight = 1250f;
+    private float newPlatformCenterX = 11.5f;
+    private float platformCenterY;
+    public GameObject newPlatform, platform;
+    // Use this for initialization
+    void Start () {
+		platformCenterY= GameObject.Find("FirstPlatform").transform.position.y;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 move = new Vector3(3, 0, 0);
+        Vector3 move = new Vector3(5, 0, 0);
         gameObject.transform.Translate(move * Time.deltaTime);
+        CreateNewPlatform();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
@@ -21,5 +25,13 @@ public class Runner : MonoBehaviour {
     void Jump()
     {
         gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpHeight);
+    }
+    void CreateNewPlatform()
+    {
+        if(newPlatformCenterX-gameObject.transform.position.x < 15)
+        {
+            platform = (GameObject)Instantiate(newPlatform, new Vector3(newPlatformCenterX, platformCenterY, 0F), Quaternion.identity);
+            newPlatformCenterX = newPlatformCenterX + 5;
+        }
     }
 }
