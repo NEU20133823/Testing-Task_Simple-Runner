42ba9ac909555b2137024838e6a260ba7141a938
Sitan Gao <sitangao@usc.edu>
2019-05-16 18:24:39 -0700

    Destroy platform when out of range from left

---
+++ Assets/Scripts/DestroyPlatform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlatform : MonoBehaviour {
+    private GameObject runner;
+	// Use this for initialization
+	void Start () {
+		runner= GameObject.FindGameObjectWithTag("Runner");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (runner.transform.position.x - gameObject.transform.position.x > 15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
